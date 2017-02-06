7ec95b2f098e73636551682a3d8d60ceec804bf9
Tim Thacher <tthacher@Tims-iMac.local>
2017-02-06 17:38:41 -0600

    new folders and files

---
+++ angular/module1-solution/app.js
@@ -0,0 +1,31 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+
+  $scope.checkList = function() {
+    if(!$scope.userInput || !$scope.userInput.trim()) {
+      $scope.outputMsg = "Please enter data first.";
+    }
+    else if(calculateNumberOfItems($scope.userInput) < 4){
+      $scope.outputMsg = "Enjoy!";
+    }
+    else {
+      $scope.outputMsg = "Too Much!";
+    }
+  }
+}
+
+function calculateNumberOfItems(inputString) {
+  return inputString.split(",").filter(isValidItem).length;
+}
+
+function isValidItem(inputString) {
+  return inputString.trim()
+}
+
+})();
